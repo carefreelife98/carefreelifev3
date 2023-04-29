2c2086f05c322fb34387af61e560c8fde40112d0
CarefreeLife <csm4903@naver.com>
2023-04-29 18:14:40 +0900

    first commit v3

---
+++ src/main/java/carefreelife/CarefreeLifeV3/CarefreeLifeV3Application.java
@@ -0,0 +1,13 @@
+package carefreelife.CarefreeLifeV3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarefreeLifeV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarefreeLifeV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/carefreelife/CarefreeLifeV3/CarefreeLifeV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package carefreelife.CarefreeLifeV3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarefreeLifeV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
